eced673169eddd99d318ff3333b64ff820230b82
Linkyolo <alexjit95@gmail.com>
2025-07-27 16:58:39 +0200

    Added a temp layout and icons

---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Navigation from "../components/ui/Navigation.vue";
+</script>
+<template>
+  <UContainer class="flex flex-col h-screen">
+    <Navigation class="flex-none" />
+    <div class="grow">
+      <slot />
+    </div>
+    <!---  for the future  <Footer class="flex-none" /> --->
+  </UContainer>
+</template>
